4bcc20e2a5b6f3c8582fddf5f3267cdf34337d22
Nathan! <nathan.vanblaere@thrives.be>
2018-08-18 12:51:37 +0200

    Added models and repository interface

---
+++ XrmToolBoxHackaton.XrmSanityCheck/XrmToolBoxHackaton.XrmSanityCheck/Models/CheckListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XrmToolBoxHackaton.XrmSanityCheck.Models
+{
+    public class CheckListItem
+    {
+        public Guid? Id { get; set; }
+        public Guid? PublicId { get; set; }
+        public string Title { get; set; }
+        public bool IsChecked { get; set; }
+        public DateTime? CheckedOn { get; set; }
+    }
+}
---
+++ XrmToolBoxHackaton.XrmSanityCheck/XrmToolBoxHackaton.XrmSanityCheck/Repository/ICheckListRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XrmToolBoxHackaton.XrmSanityCheck.Models;
+
+namespace XrmToolBoxHackaton.XrmSanityCheck.Repository
+{
+    public interface ICheckListRepository
+    {
+        #region CheckList Members
+        IEnumerable<CheckList> GetCheckLists();
+
+        Guid CreateCheckList(CheckList list);
+        void UpdateCheckList(CheckList list);
+        void DeleteCheckList(CheckList list);
+        #endregion
+
+        #region CheckListItem Members
+        IEnumerable<CheckList> GetCheckListItems(Guid checklistItemId);
+        Guid CreateCheckListItem(CheckListItem item);
+        void UpdateCheckListItem(CheckListItem item);
+        void DeleteCheckListItem(CheckListItem item);
+
+        #endregion
+    }
+}
